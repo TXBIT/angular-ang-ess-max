544d3205c394259a516774d378618b5ae432268e
TXBIT <shenlong203@gmail.com>
2020-08-11 15:52:23 +0700

    005 Cart Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,16 @@ import { Component } from "@angular/core";
 export class AppComponent {
   title = "app";
   rootName = "username001";
+  rootItems = ["Apples", "Bananas", "Cherries"];
 
-  onNameChanged(newName: string) {
+  onNameChanged(newName: any) {
     this.rootName = newName;
+  }
+
+  onItemWasAdded(newItem: any) {
+    // this.rootItems.push(newItem);
+    // this.rootItems = this.rootItems.concat([newItem]);
+    this.rootItems = [...this.rootItems, newItem];
+    console.log(this.rootItems);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from "./app.component";
 import { UserComponent } from "./user.component";
 import { UserDetailComponent } from "./user-detail/user-detail.component";
 import { DashboardComponent } from "./dashboard.component";
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { DashboardComponent } from "./dashboard.component";
     UserComponent,
     DashboardComponent,
     UserDetailComponent,
+    CartComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
